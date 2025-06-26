a184cc7bff92f611459b531c0c2df78b990f033c
VeeJ <vesy17032003@gmail.com>
2025-06-26 11:04:05 +0700

    refactor(ChatComposer): comment out emoji picker toggle click handler

--- src/components/chat/ChatComposer.tsx
+++ src/components/chat/ChatComposer.tsx
@@ -130,7 +130,7 @@ const ChatComposer = ({ roomId }: { roomId: string }) => {
           />
         ) : (
           <Eclipse
-            onClick={() => setShowEmojiPicker((prev) => !prev)}
+            // onClick={() => setShowEmojiPicker((prev) => !prev)}
             className="text-[#858585] cursor-default"
             size={30}
           />
