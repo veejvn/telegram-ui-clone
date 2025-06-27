dc753a168806c34cae88dce97a4f25342c5d073e
T-Trieu <trieuntce180675@fpt.edu.vn>
2025-06-27 21:49:14 +0700

    Remove CallOverlay component from RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/app/provider";
-<<<<<<< HEAD
 import CallOverlay from "@/components/call/CallOverlay"; // Thêm dòng này
-=======
 import { Toaster } from "sonner";
->>>>>>> 63ff68dd67bfe39f4b7a7c4bfe8ad19fa282377a
 
 const inter = Inter({
   subsets: ["vietnamese"],
@@ -26,11 +23,8 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} antialiased`}>
         <Providers>
-<<<<<<< HEAD
           <CallOverlay />  {/*  Luôn lắng nghe sự kiện call */}
-=======
           <Toaster richColors position="top-center" />
->>>>>>> 63ff68dd67bfe39f4b7a7c4bfe8ad19fa282377a
           {children}
         </Providers>
       </body>
