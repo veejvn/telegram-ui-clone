b92f10893f3830a24441ee803da8b95fe8cccf37
VeeJ <vesy17032003@gmail.com>
2025-07-03 10:06:18 +0700

    Refactor next.config.ts for improved readability and consistency

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,46 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactStrictMode: true,
-    transpilePackages: ['matrix-js-sdk'],
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'matrix.org',
-                pathname: '/_matrix/media/**',
-            },
-            {
-                protocol: 'https',
-                hostname: '*.matrix.org',
-            },
-            {
-                protocol: 'https',
-                hostname: 'matrix-client.matrix.org',
-            },
-        ],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
+  reactStrictMode: true,
+  transpilePackages: ['matrix-js-sdk'],
+  images: {
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'matrix.org',
+              pathname: '/_matrix/media/**',
+          },
+          {
+              protocol: 'https',
+              hostname: '*.matrix.org',
+          },
+          {
+              protocol: 'https',
+              hostname: 'matrix-client.matrix.org',
+          },
+      ],
+  dangerouslyAllowSVG: true,
+  contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  eslint: {
+      ignoreDuringBuilds: true,
+  },
+  assetPrefix: '/chat-static',
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/chat-static/_next/:path+',
+          destination: '/_next/:path+',
+        },
+      ],
+    }
+  },
+  // experimental: {
+  //   serverActions: {
+  //     allowedOrigins: ['http://localhost:3000'],
+  //   },
+  // },
 };
 
 export default nextConfig;
