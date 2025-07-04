e1e43a1e70894ee7e095ee40edb66d2f5ae55b0d
T-Trieu <trieuntce180675@fpt.edu.vn>
2025-07-04 20:31:03 +0700

    layout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,55 +1,10 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { useRouter, usePathname } from "next/navigation";
-import { ROUTES } from "@/constants/routes";
-import { useAuthStore } from "@/stores/useAuthStore";
-import LoadingSpinner from "@/components/common/LoadingSpinner";
-import BottomNavigattion from "@/components/layouts/BottomNavigation";
-import { MatrixClientProvider } from "@/contexts/MatrixClientProvider";
-import { getLS } from "@/tools/localStorage.tool";
+import ProtectedClientLayout from '../../components/layouts/ProtectedClientLayout.client';
 
 export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
-  const { isLogging } = useAuthStore();
-  const [isReady, setIsReady] = useState(false);
-
-  const statusBarHeight = getLS("statusBarHeight");
-
-  const mainStyle = {
-    paddingTop: statusBarHeight ? Number(statusBarHeight) : 0,
-  };
-
-  useEffect(() => {
-    setIsReady(true);
-    if (!isLogging) {
-      router.replace(ROUTES.LOGIN);
-    }
-  }, [isLogging, router]);
-
-  const pathname = usePathname();
-  const isChatDetailPage = pathname ? /^\/chat(\/.+)+$/.test(pathname) : false;
-  const isSettingPage = pathname ? pathname.startsWith("/setting/") : false;
-  const shouldShowBottomNav = !isChatDetailPage && !isSettingPage;
-
-  if (!isReady || !isLogging) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <LoadingSpinner />
-      </div>
-    );
-  }
-  return (
-    <MatrixClientProvider>
-      <main>
-        <div style={mainStyle} className="flex flex-col min-h-screen">
-          {children}
-          {shouldShowBottomNav && <BottomNavigattion />}
-        </div>
-      </main>
-    </MatrixClientProvider>
-  );
+  // Đây là server component, không import matrix-js-sdk
+  return <ProtectedClientLayout>{children}</ProtectedClientLayout>;
 }
