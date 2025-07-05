30a82b2866df962cf60fb2c05775ba0316f46476
VeeJ <vesy17032003@gmail.com>
2025-07-05 15:07:40 +0700

    hinden log

--- src/contexts/MatrixClientProvider.tsx
+++ src/contexts/MatrixClientProvider.tsx
@@ -30,8 +30,6 @@ export function MatrixClientProvider({
       const userId = getCookie("matrix_user_id");
       const deviceId = getCookie("matrix_device_id");
 
-      console.log(accessToken, userId, deviceId);
-
       if (!accessToken || !userId || !deviceId) return;
 
       try {
