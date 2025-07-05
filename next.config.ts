57e69b9b45cb88fdc94ddad16ebdabbb51e5ba3d
VinhTran1726 <vinhtqce181347@fpt.edu.vn>
2025-07-05 21:10:35 +0700

    adjust setting appearance ui (#92)
    Co-authored-by: VeeJ <vesy17032003@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -2,31 +2,59 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
-    transpilePackages: ['matrix-js-sdk'],
+    transpilePackages: ["matrix-js-sdk"],
     images: {
         remotePatterns: [
             {
-                protocol: 'https',
-                hostname: 'matrix.org',
-                pathname: '/_matrix/media/**',
+                protocol: "https",
+                hostname: "matrix.org",
+                pathname: "/_matrix/media/**",
             },
             {
-                protocol: 'https',
-                hostname: '*.matrix.org',
+                protocol: "https",
+                hostname: "*.matrix.org",
             },
             {
-                protocol: 'https',
-                hostname: 'matrix-client.matrix.org',
+                protocol: "https",
+                hostname: "matrix-client.matrix.org",
             },
         ],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+        dangerouslyAllowSVG: true,
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     },
     eslint: {
         ignoreDuringBuilds: true,
     },
+
+    // 👇 Cấu hình thêm webpack loader cho SVG
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            issuer: /\.[jt]sx?$/,
+            use: [
+                {
+                    loader: "@svgr/webpack",
+                    options: {
+                        icon: true,
+                        svgoConfig: {
+                            plugins: [
+                                {
+                                    name: "preset-default",
+                                    params: {
+                                        overrides: {
+                                            removeViewBox: false,
+                                        },
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                },
+            ],
+        });
+
+        return config;
+    },
 };
 
 export default nextConfig;
-
-
