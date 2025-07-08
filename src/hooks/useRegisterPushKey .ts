c29639f60aff63a762d498708a1293a94f1557ce
Nguyễn Hoàng Vệ <129590488+veejvn@users.noreply.github.com>
2025-07-08 14:55:29 +0700

    Fix push token feature (#127)
    * Refactor useRegisterPushKey hook to improve push token registration logic. Ensure accessToken is validated before making the fetch request and update the API endpoint for push notifications. Clean up console log statements for better code clarity.
    
    * Update useRegisterPushKey hook to utilize environment variable for push token domain, enhancing flexibility in API endpoint configuration.

--- src/hooks/useRegisterPushKey .ts
+++ src/hooks/useRegisterPushKey .ts
@@ -4,44 +4,48 @@ import { getLS, setLS } from "@/tools/localStorage.tool";
 import { useEffect } from "react";
 
 const useRegisterPushKey = (accessToken: string | null) => {
+
+  const PUSH_TOKEN_DOMAIN = process.env.NEXT_PUBLIC_PUSH_TOKEN_DOMAIN  || ""
+
   useEffect(() => {
     if (typeof window === "undefined") return;
 
     const pushToken = getLS("pushToken");
-    //console.log("pushToken", pushToken)
-    if (!pushToken) return;
 
-    const lastRegistered = getLS("lastPushToken");
-    if (lastRegistered === pushToken) return;
-
-    const payload = {
-      app_display_name: "Ting Tong",
-      app_id: "ting.tong.app",
-      device_display_name: "Ting Tong",
-      kind: "http",
-      lang: "en_US",
-      pushkey: pushToken,
-    };
-
-    fetch("https://matrix-synapse-service.blocktrend.xyz/api/v1/pushers", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`, // Token từ client chat
-      },
-      body: JSON.stringify(payload),
-    })
+    if (pushToken && accessToken){
+      const lastRegistered = getLS("lastPushToken");
+      if (lastRegistered === pushToken) return;
+  
+      const payload = {
+        app_display_name: "Ting Tong",
+        app_id: "ting.tong.app",
+        device_display_name: "Ting Tong",
+        kind: "http",
+        lang: "en_US",
+        pushkey: pushToken,
+      };
+  
+      fetch(PUSH_TOKEN_DOMAIN, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`, // Token từ client chat
+        },
+        body: JSON.stringify(payload),
+      })
       .then((res) => {
         if (!res.ok) throw new Error("Pushkey registration failed");
         return res.json();
       })
       .then((data) => {
         setLS("lastPushToken", pushToken);
-        console.log("Pushkey registered:", data);
+        //console.log("Pushkey registered:", data);
       })
       .catch((err) => {
         console.error("Pushkey error:", err);
       });
+    }
+
   }, [accessToken]);
 };
 
