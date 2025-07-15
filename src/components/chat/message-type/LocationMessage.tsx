1e0c60197a261d32b8a21e69cc593cdea89e5f6c
VeeJ <vesy17032003@gmail.com>
2025-07-15 21:55:07 +0700

    Enhance LocationMessage component with loading state and styling updates
    - Added loading state for the map image in the LocationMessage component to improve user experience during image loading.
    - Updated background color of the message container for better visibility.
    - Refactored image handling to manage loading and error states effectively.

--- src/components/chat/message-type/LocationMessage.tsx
+++ src/components/chat/message-type/LocationMessage.tsx
@@ -1,11 +1,14 @@
-import { BubbleTail } from "@/components/chat/message-type/BubbleTail";
+"use client";
+
 import { MessagePros } from "@/types/chat";
 import { formatMsgTime } from "@/utils/chat/formatMsgTime";
 import clsx from "clsx";
 import { Check, CheckCheck, MapPin } from "lucide-react";
+import { useState } from "react";
 
 export function LocationMessage({ msg, isSender }: MessagePros) {
   const { location } = msg;
+  const [isImgLoading, setIsImgLoading] = useState(true);
   const mapLink = `https://www.google.com/maps/search/?api=1&query=${location?.latitude},${location?.longitude}`;
   const textClass = clsx(
     "flex items-center gap-1 text-xs",
@@ -20,25 +23,34 @@ export function LocationMessage({ msg, isSender }: MessagePros) {
           isSender ? "ml-auto" : "mr-auto"
         )}
       >
-        <div className="h-40 w-80 rounded-2xl overflow-hidden shadow relative bg-[rgb(13,71,161)]">
+        <div className="h-40 w-80 rounded-2xl overflow-hidden shadow relative bg-white">
           <a
             href={mapLink}
             target="_blank"
             rel="noopener noreferrer"
             //className="flex"
           >
-            {/* <img
+            {isImgLoading && (
+              <div className="absolute inset-0 flex items-center justify-center bg-white">
+                {/* Spinner hoặc text loading */}
+                <span className="">Loading...</span>
+              </div>
+            )}
+            <img
               src={`/chat/api/map-static?lat=${location?.latitude}&lng=${location?.longitude}`}
               alt="image"
               className="h-40 w-80 rounded-2xl"
-            /> */}
-            <div className="h-40 w-80 rounded-2xl flex justify-center items-center">
+              onLoad={() => setIsImgLoading(false)}
+              onError={() => setIsImgLoading(false)}
+              style={isImgLoading ? { visibility: "hidden" } : {}}
+            />
+            {/* <div className="h-40 w-80 rounded-2xl flex justify-center items-center">
               <MapPin className="size-20 text-white" />
-            </div>
+            </div> */}
           </a>
           {msg.time && (
             <div
-              className={`absolute bottom-1 bg-[#0d47a1] text-white text-xs px-2 py-1 rounded-full ${
+              className={`absolute bottom-1 bg-zinc-600 text-white text-xs px-2 py-1 rounded-full ${
                 isSender ? "left-2" : "right-2"
               }`}
             >
