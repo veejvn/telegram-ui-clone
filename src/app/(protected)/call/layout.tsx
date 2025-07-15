6b426633c29aa74f17fd034975c4474203b43abc
Nhatphiinah <153935112+Nhatphiinah@users.noreply.github.com>
2025-07-15 15:23:12 +0700

    Callid (#181)
    * Action Call v3
    
    * can build
    
    * can build hihi

--- src/app/(protected)/call/layout.tsx
+++ src/app/(protected)/call/layout.tsx
@@ -1,24 +1,9 @@
 // src/app/(protected)/call/layout.tsx
-"use client";
 
-import React, { Suspense, useEffect, useState } from "react";
-import { useAuthStore } from "@/stores/useAuthStore";
-import { callService } from "@/services/callService";
-import GetCookie from "@/components/auth/GetCookie";
+import CallClientLayout from "@/components/call/CallClientLayout";
+
 
 export default function CallLayout({ children }: { children: React.ReactNode }) {
-  const [checked, setChecked] = useState(false);
-  const accessToken = useAuthStore((state) => state.accessToken);
-  const userId = useAuthStore((state) => state.userId);
-  const deviceId = useAuthStore((state) => state.deviceId);
 
-  useEffect(() => {
-    if (accessToken && userId && deviceId) {
-      setChecked(true);
-      callService.reinitialize();
-    }
-  }, [accessToken, userId, deviceId]);
-
-  if (!checked) return <GetCookie />;
-  return <main className="min-h-screen flex flex-col">{children}</main>;
+  return <CallClientLayout>{children}</CallClientLayout>;
 }
