2d67f2fd034e54120e55cb4a73b8b3da3a19cdff
Nguyễn Hoàng Vệ <129590488+veejvn@users.noreply.github.com>
2025-07-20 01:17:51 +0700

    Feat refactor on timeline (#212)
    * Enhance chat functionality with file and audio message support
    
    - Updated ChatComposer to streamline file information handling when sending messages.
    - Refactored useTimeline hook to include additional metadata for audio messages and improved read status management.
    - Introduced new properties for file and audio message types in the chat store, enhancing message data structure.
    - Improved handling of message types and metadata extraction for better user experience.
    
    Co-authored-by: VeeJ <vesy17032003@gmail.com>
    
    * Refactor chat components to enhance image and video message handling
    
    - Updated ChatComposer to support sending multiple images and videos, including dimension extraction for images.
    - Enhanced ImageMessage and VideoMessage components to display loading states while media is being fetched.
    - Improved metadata handling for image and video messages in the chat store and services.
    - Refactored useTimeline hook to accommodate new image and video information structures.
    
    Co-authored-by: VeeJ <vesy17032003@gmail.com>

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -1,20 +1,29 @@
 import { Message } from "@/stores/useChatStore";
 
-export interface MessagePros { msg: Message, isSender: boolean, animate?: boolean }
-
-export interface ChatEditButtonPros{ 
-    isEditMode: boolean, 
-    onEdit: () => void, 
-    onDone: () => void,
-    className?: string
+export interface MessagePros {
+  msg: Message;
+  isSender: boolean;
+  animate?: boolean;
 }
 
-export interface LocationInfo{
-    geoUri: string,
-    displayText: string
+export interface ChatEditButtonPros {
+  isEditMode: boolean;
+  onEdit: () => void;
+  onDone: () => void;
+  className?: string;
 }
 
-export interface FileInfo{
-    fileSize: number,
-    mimeType: string
+export interface LocationInfo {
+  geoUri: string;
+  displayText: string;
+}
+
+export interface FileInfo {
+  fileSize: number;
+  mimeType: string;
+}
+
+export interface ImageInfo {
+  width: number;
+  height: number;
 }
